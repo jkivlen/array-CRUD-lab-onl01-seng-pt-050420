c24c3ec98d9d4b9b120787b6bae83f9223bb5e3d
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-09 21:39:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["ice", "bye", "try", "jump"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   ["ice", "jump"]
+   << "zero"
 end
 
 def add_element_to_start_of_array(array, element)
